fae4c35bbff8a6d7f12a79b9a94a2198fe108f39
Egor <e.moisei1@gmail.com>
2023-02-11 20:11:55 +0300

    add id for each char

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -31,7 +31,6 @@ class RandomChar extends Component {
 
   onCharLoading = (char) => {
     this.setState({
-      char,
       loading: false,
     });
   };
@@ -45,7 +44,7 @@ class RandomChar extends Component {
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    //this.onCharLoading();
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
@@ -82,14 +81,22 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
-  let imgStyle = {'objectFit' : 'cover'};
-  if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'){
-    imgStyle = {'objectFit' : 'contain'};
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
   }
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
