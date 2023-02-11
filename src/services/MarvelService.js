8ae65a1fe9ff75599ae6a112e4dd5ebdf7ec39e2
Egor <e.moisei1@gmail.com>
2023-02-11 16:13:58 +0300

    add randomChar and refactor some little details

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
-    _apiBase='https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=757bf3cd539ab20ba9a2c64bc414608f';
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=757bf3cd539ab20ba9a2c64bc414608f";
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -12,15 +12,27 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
-  getCharacter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const res = await this.getResource(
       `${this._apiBase}characters/${id}?${this._apiKey}`
     );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? `${char.description.slice(0,210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
