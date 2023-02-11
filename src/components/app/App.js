34b2373af6f2638d223867eb7c0180937f86295a
Egor <e.moisei1@gmail.com>
2023-02-11 20:07:42 +0300

    add change Chars on btn and charList with chars

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,39 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
-import decoration from '../../resources/img/vision.png';
+import decoration from "../../resources/img/vision.png";
 
-const App = () => {
+class App extends Component {
+  state = {
+    showPandomChar: true,
+  };
+
+  toggleRandomChar = () => {
+    this.setState((state) => {
+      return {
+        showPandomChar: !state.showPandomChar,
+      };
+    });
+  };
+
+  render() {
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+      <div className="app">
+        <AppHeader />
+        <main>
+          {this.state.showPandomChar ? <RandomChar /> : null}
+          <div className="char__content">
+            <CharList />
+            <CharInfo />
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,7 +8,6 @@ import mjolnir from "../../resources/img/mjolnir.png";
 class RandomChar extends Component {
   constructor(props) {
     super(props);
-    this.updateChar();
   }
   state = {
     char: {},
@@ -18,8 +17,23 @@ class RandomChar extends Component {
 
   marvelService = new MarvelService();
 
+  componentDidMount() {
+    this.updateChar();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   onCharLoaded = (char) => {
     this.setState({ char, loading: false });
+  };
+
+  onCharLoading = (char) => {
+    this.setState({
+      char,
+      loading: false,
+    });
   };
 
   onError = () => {
@@ -31,6 +45,7 @@ class RandomChar extends Component {
 
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    //this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
@@ -41,7 +56,7 @@ class RandomChar extends Component {
     const { char, loading, error } = this.state;
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? <View char={char}/> : null;
+    const content = !(loading || error) ? <View char={char} /> : null;
 
     return (
       <div className="randomchar">
@@ -55,7 +70,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button onClick={this.updateChar} className="button button__main">
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -67,10 +82,14 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = {'objectFit' : 'cover'};
+  if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'){
+    imgStyle = {'objectFit' : 'contain'};
+  }
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
